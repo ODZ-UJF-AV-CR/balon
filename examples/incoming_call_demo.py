660bce10dc808a62f4fece0f89038b61af04f919
Vaclav Stepan <stepan@ujf.cas.cz>
2015-11-25 17:11:39 +0000

    Incoming call demo modified to send an SMS as response

--- examples/incoming_call_demo.py
+++ examples/incoming_call_demo.py
@@ -1,58 +1,52 @@
 #!/usr/bin/env python
 
 """\
-Demo: handle incoming calls
+Handle incoming calls by replying with an SMS
 
-Simple demo app that listens for incoming calls, displays the caller ID,
-optionally answers the call and plays sone DTMF tones (if supported by modem), 
-and hangs up the call.
 """
 
-from __future__ import print_function
+#from __future__ import print_function
 
 import time, logging
 
 PORT = '/dev/ttyACM99'
-BAUDRATE = 115200
+BAUDRATE = 9600
 PIN = None # SIM card PIN (if any)
 
-from gsmmodem.modem import GsmModem
-from gsmmodem.exceptions import InterruptedException
+from gsmmodem.modem import GsmModem, SentSms
+from gsmmodem.exceptions import InterruptedException, PinRequiredError, IncorrectPinError, TimeoutException
 
 def handleIncomingCall(call):
+    alreadyResponded=False
     if call.ringCount == 1:
-        print('Incoming call from:', call.number)
-    elif call.ringCount >= 2:
-        if call.dtmfSupport:
-            print('Answering call and playing some DTMF tones...')
-            call.answer()
-            # Wait for a bit - some older modems struggle to send DTMF tone immediately after answering a call
-            time.sleep(2.0) 
-            try:
-                call.sendDtmfTone('9515999955951')
-            except InterruptedException as e:
-                # Call was ended during playback
-                print('DTMF playback interrupted: {0} ({1} Error {2})'.format(e, e.cause.type, e.cause.code))                
-            finally:
-                if call.answered:
-                    print('Hanging up call.')
-                    call.hangup()
-        else:            
-            print('Modem has no DTMF support - hanging up call.')
-            call.hangup()
+        print 'Incoming call from: {0}'.format(call.number)
+    elif not (call.number is None):
+        alreadyResponded=True
+        try:
+           sms = modem.sendSms(call.number, 'Text')
+        except TimeoutException:
+           print 'Failed to send message: the send operation timed out'
+        else:
+           if sms.report:
+                print 'Message sent{0}'.format(' and delivered OK.' if sms.status == SentSms.DELIVERED else ', but delivery failed.')
+           else:
+                print 'Message sent.'
     else:
-        print(' Call from {0} is still ringing...'.format(call.number))
+        print ' Call from {0} is still ringing...'.format(call.number)
     
 def main():
-    print('Initializing modem...')
+    print 'Initializing modem...'
     #logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+    global modem
     modem = GsmModem(PORT, BAUDRATE, incomingCallCallbackFunc=handleIncomingCall)
-    modem.connect(PIN)
-    print('Waiting for incoming calls...')
-    try:    
-        modem.rxThread.join(2**31) # Specify a (huge) timeout so that it essentially blocks indefinitely, but still receives CTRL+C interrupt signal
-    finally:
-        modem.close()
+    while True:
+      modem.connect(PIN)
+      print 'Waiting for incoming calls...'
+      try:    
+	modem.rxThread.join(10) # Specify a (huge) timeout so that it essentially blocks indefinitely, but still receives CTRL+C interrupt signal
+      finally:
+	modem.close()
+	print "Reopening modem..."
 
 if __name__ == '__main__':
     main()
