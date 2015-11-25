7170fb27518cd91ad77b72129f7f3331dfb2d683
Vaclav Stepan <stepan@ujf.cas.cz>
2015-11-25 17:48:44 +0000

    Capture images in a loop indefinitely

--- examples/cam.sh
+++ examples/cam.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-fswebcam -r 1600x1200 -D 1 -S 10 -F 5 --save shot.jpg && qiv shot.jpg
+# Acquire images from the webcam in a loop
+
+DELAY=30
+RES=1600x1200
+RES=1280x960
+
+# F 5 is better at night
+mkdir -p img
+fswebcam -r $RES -D 1 -S 10 -F 1 -l $DELAY --gmt --timestamp "%F %T" --save "img/%F-%T.jpg"
