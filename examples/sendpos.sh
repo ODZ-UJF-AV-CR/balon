3992b2bc8c7e84cd9767729d877e26d872a4cf97
Vaclav Stepan <stepan@ujf.cas.cz>
2015-11-25 17:12:41 +0000

    Send current position using shell script

--- examples/sendpos.sh
+++ examples/sendpos.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 #SYSRTC=$( date "+%Y-%m-%d %H:%M:%S" )
-GPSDATA=$(./gpsline.py)
 
-echo Got GPSdata
+# Get line with GPS coordinates
+GPSDATA=$(timeout -k 10s 30s ./gpsline.py)
+SENDTIMEOUT="60s"
+DEST=+420777642401
 
-echo "$GPSDATA" | gammu sendsms TEXT +420777642401
+if [ "x$GPSDATA" == "x" ]; then
+   echo No fix, no fun.
+else 
+   echo Got GPSdata: $GPSDATA
+   echo Trying to send for $SENDTIMEOUT
+   echo $GPSDATA | timeout -k 5s $SENDTIMEOUT gammu sendsms TEXT $DEST 
+fi
