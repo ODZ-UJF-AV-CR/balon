560b2abfe65825307b89c68d57133fa396ba59dc
kakl <kakl@astro.cz>
2015-12-02 04:02:15 +1030

    first version with cooperation with PIC

--- spi/testspi.py
+++ spi/testspi.py
@@ -11,7 +11,7 @@ GPIO_EXPORT_PATH=os.path.normpath('/sys/class/gpio/export')
 
 
 
-def writepin(pin_value):
+def writepins(pin_value):
     pin = open("/sys/class/gpio/gpio204/value","w")
     pin.write(pin_value)
     pin.close()
@@ -36,19 +36,21 @@ try:
     #spi.mode = 2
     #spi.bits_per_word = 8
     #spi.cshigh = False
-    writepin("1")
     while True:
         #resp = spi.readbytes(2)
         #resp = spi.xfer([0xAA,0x55,0xAA,0x55],10000000,0) # transfer one byte
-        for n in range(7000):
-            writepin("0")
-            #writepin("0")
-            time.sleep(0.0001)  # cca 250 us
-            pin = open("/sys/class/gpio/gpio204/value","w")
-            pin.write("1")
+        for n in range(20):
+            #pin = open(GPIO_PIN_PATH, "w")
+            #resp = spi.xfer([0x00,0x00]) # transfer
+            #pin.write("1")
+            #pin.close()
+            #pin = open(GPIO_PIN_PATH, "w")
+            #pin.write("0")
+            #pin.close()
             resp = spi.readbytes(2)
-            pin.close()
-        time.sleep(0.1) # sleep for 0.1 seconds
+            time.sleep(0.00001) # sleep for 0.1 seconds
+            #print resp
+        #time.sleep(0.1) # sleep for 0.1 seconds
         #end while
 except KeyboardInterrupt: 
     spi.close()
