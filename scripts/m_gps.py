3c8057cad661490d4cf2eb98c9a8f391f971773d
Vaclav Stepan <stepan@ujf.cas.cz>
2015-12-14 03:14:57 +0000

    GSM, GPS a PCRD moduly

--- scripts/m_gps.py
+++ scripts/m_gps.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
-# Monitor script GPS part
-# Adapted from code by Dan Mandle http://dan.mandle.me September 2012, License: GPL 2.0 
+# Monitor script GPS part, communicates with gpsd and acquires time
 
 import time
 import datetime
@@ -11,65 +10,86 @@ import re
 
 import os
 from gps import *
-from time import *
 import time
 import threading
 
 gpsd = None #seting the global variable
 
-# Logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s [%(levelname)s] (%(threadName)-10s) %(message)s',
-                    )
+# Dictionaries for last read and last valid data
+data = {}
+lv_data = {}
 
-#### GPS Poller #####################################################
-class GpsPoller(threading.Thread):
-  def __init__(self):
-    logging.info("Starting GPS poller thread")
-    threading.Thread.__init__(self)
-    global gpsd #bring it in scope
-    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-    self.current_value = None
-    self.running = True #setting the thread running to true
-
-  def run(self):
-    logging.info("GPS poller thread running")
-    global gpsd
-    while gpsp.running:
-      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
-
-# GPS thread initialization and startup
-gpsp = GpsPoller() # create the thread
-gpsp.start() # start it up
-
-# Get sensor value or -1 if not available
-def sv(sname):
-  if sname in sensors:
-    return(sensors[sname])
+def dv(sname):
+  if sname in data:
+    return(data[sname])
   else:
     return(-1)
 
-def get_gpsdata():
-  # GPS data 
-  logging.debug("Retrieving: GPS data")
-            
-  sensors['GPS_Time']=gpsd.utc
-  sensors['GPS_Fix']=gpsd.fix.mode
-  sensors['GPS_Alt']=gpsd.fix.altitude
-  sensors['GPS_Lat']=gpsd.fix.latitude
-  sensors['GPS_Lon']=gpsd.fix.longitude
-  sensors['GPS_Speed']=gpsd.fix.speed
-  sensors['GPS_Climb']=gpsd.fix.climb 
+def lv(sname):
+  if sname in lv_data:
+    return(lv_data[sname])
+  else:
+    return(-1)
+
+#### GPS Poller #####################################################
+class GpsPoller(threading.Thread):
+  global lv_data
+  global data
+
+  def __init__(self):
+    logging.info("GPS poller thread initializing.")
+    threading.Thread.__init__(self)
+    global gpsd #bring it in scope
+    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    self.running = True #setting the thread running to true
+
+  def run(self):
+    logging.info("GPS poller thread running.")
+    global gpsd
+    while self.running:
+      #logging.debug("Retrieving GPS data.")
+      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+      data['GPS_Time']=gpsd.utc
+      data['GPS_Fix']=gpsd.fix.mode
+      data['GPS_Alt']=gpsd.fix.altitude
+      data['GPS_Lat']=gpsd.fix.latitude
+      data['GPS_Lon']=gpsd.fix.longitude
+      data['GPS_Speed']=gpsd.fix.speed
+      if (data['GPS_Fix'] > 2):
+        for k in data.keys():
+          lv_data[k] = data[k]  
+      #logging.info(self.get_status_string())
+      #logging.debug(self.get_lv_status_string())
+
+  def get_status_string(self):
+    status_string = "GPSTime: %s Fix: %d Alt: %.1f m Speed: %.1f m/s Lat: %f Lon: %f " % (dv('GPS_Time'), dv('GPS_Fix'), dv('GPS_Alt'), dv('GPS_Speed'), dv('GPS_Lat'), dv('GPS_Lon'))
+    return(status_string)
+
+  def get_lv_status_string(self):
+    status_string = "LV GPSTime: %s Fix: %d Alt: %.1f m Speed: %.1f m/s Lat: %f Lon: %f " % (lv('GPS_Time'), lv('GPS_Fix'), lv('GPS_Alt'), lv('GPS_Speed'), lv('GPS_Lat'), lv('GPS_Lon'))
+    return(status_string)
+
+  def get_record(self):
+    return(str("%s\t%d\t%.1f\t%.1f\t%.1f\t%f\t%f\t" % (str(dv('GPS_Time')), dv('GPS_Fix'), dv('GPS_Alt'), dv('GPS_Speed'), dv('GPS_Climb'), dv('GPS_Lat'), dv('GPS_Lon'))))
+
+  def split_gps_time(gpstime, self):
+    logging.info(lv('GPS_Time'))
 
 #### main ####
 if __name__ == '__main__':
+  # Logging
+  logging.basicConfig(level=logging.DEBUG,
+    format='%(asctime)s [%(levelname)s] (%(threadName)-10s) %(message)s',
+    )
   try:
-    while gpsp.running and get_gpsdata():
-      print Running 
+    # GPS thread initialization and startup
+    gpsp = GpsPoller() # create the thread
+    gpsp.start() # start it up
+    while gpsp.running:
+      time.sleep(1)
   except (KeyboardInterrupt, SystemExit):
     logging.error("Exiting:")
     if gpsp.running:
       gpsp.running = False
       logging.info("GPS thread asked to shut down.")
     sys.exit(0)
-
---
+++ scripts/m_gsm.py
@@ -0,0 +1,204 @@
+#!/usr/bin/python
+
+# Monitor script
+
+# GPS part from code by Dan Mandle http://dan.mandle.me September 2012, License: GPL 2.0 
+
+import time
+import datetime
+import sys
+import logging 
+import re
+import os
+import threading
+
+import m_gps
+
+## Modem
+from gsmmodem.modem import GsmModem, SentSms
+from gsmmodem.exceptions import InterruptedException, PinRequiredError, IncorrectPinError, TimeoutException
+###
+
+modem=None
+
+# GSM module #
+default_destination = "+420777642401"
+PORT = '/dev/ttyACM99'
+BAUDRATE = 9600 # Higher baud rates than 9600 lead to errors
+PIN = None # SIM card PIN (if any)
+
+#### Send position in a SMS ########################################
+def send_position_via_sms(destination):
+    logging.info("Will send position via SMS to: {0}.".format(destination))
+    # Get current time
+
+    try:
+       # Get a GPS fix, prepare a string with it
+       if (m_gps.lv('GPS_Fix') < 3):
+         timestring = time.strftime('%T', time.gmtime())
+         smstext = "{0} GSM: {1}".format(timestring, gsmpart.cellInfo) 
+       else:
+         timestring = time.strftime('%T', time.gmtime())
+         smstext = "{0} alt{1} http://www.google.com/maps/place/{2},{3}".format(timestring, m_gps.lv("GPS_Alt"), m_gps.lv("GPS_Lat"), m_gps.lv("GPS_Lon"))
+
+       #if ('Bat_Temp' in sensors) and ('Bat_Charge' in sensors):
+       #  smstext = smstext + (" BT{0} BCH{1} ".format(sv('Bat_Temp'), sv('Bat_Charge')))
+       #else:
+       #  smstext = smstext + " NoBatInfo"
+
+       sms = modem.sendSms(destination, smstext, waitForDeliveryReport=True)
+    except TimeoutException:
+       logging.warn('Failed to send message to {0}: the send operation timed out'.format(call.number))
+    else:
+       if sms.report:
+            logging.info('Message sent{0}'.format(' and delivered OK.' if sms.status == SentSms.DELIVERED else ', but delivery failed.'))
+       else:
+            logging.info('Message sent.')
+
+
+#### Incoming call handler #########################################
+def handleIncomingCall(call):
+    if call.ringCount == 1:
+        logging.info('New incoming call, waiting for CLIP.'.format(call.number))
+    elif call.ringCount <= 3 and not (call.number is None):
+        logging.info('Got CLIP, Will send position to {0}.'.format(call.number))
+        destination=call.number
+        call.hangup()
+        send_position_via_sms(destination)
+    elif call.ringCount > 3:
+        call.hangup()
+        send_position_via_sms(default_destination)
+    else:
+        logging.info('Call is ringing and we still have no CLIP.')
+         
+# Modem GPIO reset handler
+def writepins(pin_value):
+    pin = open("/sys/class/gpio/gpio204/value","w")
+    pin.write(pin_value)
+    pin.close()
+
+def exportpins():
+    logging.debug("Exporting GPIO pin")
+    GPIO_EXPORT_PATH=os.path.normpath('/sys/class/gpio/export')
+    GPIO_MODE_PATH= os.path.normpath('/sys/class/gpio/gpio204/direction')
+    if (os.path.isdir('/sys/class/gpio/gpio204')):
+      logging.debug("/sys/class/gpio/gpio204 directory already exists, export not needed")
+    else:
+      try:
+        file = open(GPIO_EXPORT_PATH, 'w')
+        file.write("204")          ## export the pin to the usermode
+        file.close()
+
+        file = open(GPIO_MODE_PATH, 'r+')
+        file.write("out")          ## make the pin as output
+        file.close()
+      except:
+        logging.error("Trouble exporting GPIO pin for modem reset.")
+
+class ModemHandler(threading.Thread):
+  # +CGED: MCC:230, MNC:  3, LAC:878c, CI:2a95,
+  CGED_REGEX = re.compile(r'^\+CGED:\s*MCC:([^,]+),\s*MNC:\s*([^,]+),\s*LAC:\s*([^,]+),\s*CI:\s*([^,]+),.*')
+  def __init__(self):
+    logging.info("Starting Modem handler thread")
+    threading.Thread.__init__(self)
+    global modem
+    self.running = True #setting the thread running to true
+    self.name = "Modem"
+    self.networkName = "none"
+    self.signalStrength = -1
+    self.cellInfo = "none"
+ 
+  def modemPowerCycle(self):
+    # Should reset the modem using hardware pin
+    exportpins()
+    writepins('0')
+    time.sleep(2)
+    writepins('1') 
+
+  def get_status_string(self):
+    return("GSM: %d %s Cell: %s " % (self.signalStrength,self.networkName, self.cellInfo))
+
+  def get_record(self):
+    return("%d\t%s\t" % (self.signalStrength, self.cellInfo))
+
+  def run(self):
+    # Initialize the GPIO interface
+    #exportpins()
+    #writepins('1')
+    
+    logging.info("Modem thread running")
+    global modem
+    rxListenLength = 5
+    init_count = 0
+    
+    while self.running and init_count > -1:
+      try:
+        init_count = init_count + 1
+        modem = GsmModem(PORT, BAUDRATE, incomingCallCallbackFunc=handleIncomingCall)
+        logging.info("Initializing modem, try {0}.".format(init_count))
+        modem.connect(PIN)
+        init_count = -1
+      except (OSError, TimeoutException):
+        # OSError means pppd is likely running
+        logging.error("Failed to initialize the GSM module.")
+        try:
+          modem.close()
+        except AttributeError:
+          True 
+        time.sleep(5)
+        #self.modemPowerCycle()
+
+    logging.info('Waiting for incoming calls...')
+    while self.running:
+      try:
+        #waitForNetworkCoverage()
+        self.signalStrength = modem.signalStrength
+        self.networkName = modem.networkName
+        #modem.write("AT+CFUN=0")
+        serving_cell=self.CGED_REGEX.match(modem.write("AT+CGED=3")[0])
+        if serving_cell:
+          mcc=serving_cell.group(1)
+          mnc=serving_cell.group(2)
+          lac=serving_cell.group(3)
+          ci=serving_cell.group(4)
+          self.cellInfo="{0}/{1}/{2}/{3}".format(mcc, mnc, lac, ci)
+
+        # Comms are handled elsewhere so we could eventually just sleep, waiting
+        #time.sleep(rxListenLength)
+        modem.rxThread.join(rxListenLength) 
+      except (InterruptedException, PinRequiredError, IncorrectPinError, TimeoutException):
+        logging.error("rxThread died: {0}".format(sys.exc_info()[0]))
+
+    modem.close()
+    logging.info("Modem closed.")
+
+#### main ####
+if __name__ == '__main__':
+  # Logging
+  logging.basicConfig(level=logging.DEBUG,
+    format='%(asctime)s [%(levelname)s] (%(threadName)-10s) %(message)s',
+    )
+  try:
+    # GPS thread initialization and startup
+    gpsp = m_gps.GpsPoller() # create the thread
+    gpsp.start() # start it up
+
+    # GSM call/sms handler
+    logging.info("Initializing GSM support.")
+    gsmpart = ModemHandler()
+    gsmpart.start()
+    while gsmpart.running:
+      # GSM module data
+      logging.info(gsmpart.get_status_string())
+      time.sleep(10)
+      send_position_via_sms(default_destination)
+  except (KeyboardInterrupt, SystemExit):
+    logging.error("Exiting.")
+    if gsmpart.running:
+      gsmpart.running = False
+      logging.info("GSM part asked to shut down.")
+    if gpsp.running:
+      logging.info("GPS part asked to shut down.")
+      gpsp.running = False
+    sys.exit(0)
+
