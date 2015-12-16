29837d4ef43e14bc9681373de3d0bc0fa94c85f6
Vaclav Stepan <stepan@ujf.cas.cz>
2015-12-16 17:06:00 +0000

    added warn msg about u limits

--- scripts/m_settings.py
+++ scripts/m_settings.py
@@ -7,3 +7,5 @@ image_dir = data_dir + 'img/'
 round_beat = 5
 
 U_lpm = [8350, 8430] # Threshold to enter and exit the low power mode, in mV
+
+print "======================= SET REAL U LIMITS ====================="
