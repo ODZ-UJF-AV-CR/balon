89cd97a840e24b72332fcebf212de21376daf326
Vaclav Stepan <stepan@ujf.cas.cz>
2015-12-16 19:23:53 +0000

    Added commented U limits

--- scripts/m_settings.py
+++ scripts/m_settings.py
@@ -6,6 +6,10 @@ image_dir = data_dir + 'img/'
 
 round_beat = 5
 
+# This is for testing, triggers lpm when external power source is disconnected
+print "======================= SET REAL U LIMITS ====================="
 U_lpm = [8350, 8430] # Threshold to enter and exit the low power mode, in mV
 
-print "======================= SET REAL U LIMITS ====================="
+# Flight thresholds:
+#U_lpm = [7000,7300]
+
