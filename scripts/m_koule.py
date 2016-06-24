6c7b6b9aa32d9c540a4db4da93f4f036a9945bfd
kakl <kakl@astro.cz>
2016-06-24 17:36:49 +0200

    Kouleriiidr v beta verzi

--- scripts/m_koule.py
+++ scripts/m_koule.py
@@ -13,26 +13,27 @@ import threading
 
 port = '/dev/ttyUSB1'
 
-connected = False
 baud = 9600
 
 serial_port = serial.Serial(port, baud, timeout=5)
 
 def handle_data(data):
-  print(str(time.time()) + data)
-  datafname = g.data_dir+"data_koule.csv"
+  print(data)
+  datafname = "data_koule.csv"
   with open(datafname, "a") as nbf:
-    nbf.write(str(time.time()) + data)
+    nbf.write(data)
   nbf.close()
 
 def read_from_port(ser):
+  connected = False
   while not connected:
     #serin = ser.read()
     connected = True
 
     while True:
-      reading = ser.readline().decode()
-      handle_data(reading)
+      reading = ser.readline().rstrip()
+      if (len(reading) > 0):
+        handle_data(str(time.time()) + ' ' + reading + '\n')
 
 thread = threading.Thread(target=read_from_port, args=(serial_port,))
 thread.start()
