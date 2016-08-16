e62f76b5f48aab0ef40d61a37dfdd201fcf68a37
kakl <martin.kakona@gmail.com>
2016-08-16 17:09:38 +0000

    mrakomer with altimeter

--- scripts/mrakomer.py
+++ scripts/mrakomer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Non-threaded NB poller
+# Mrakomer poller
 # 
 
 import m_settings as g
@@ -10,31 +10,65 @@ import logging
 import serial
 import math
 import threading
+from pymlab import config
 
-port = '/dev/ttyUSB1'
 
-baud = 9600
+port = '/dev/ttyUSB0'
+
+baud = 2400
 
 serial_port = serial.Serial(port, baud, timeout=5)
 
+cfg = 	config.Config(
+		    i2c = {
+		        "port": 1,
+		    },
+		    bus = [
+		        {
+		            "name":          "altimet",
+		            "type":        "altimet01",
+		        },
+		    ],
+		)
+
+cfg.initialize()
+alt = cfg.get_device("altimet")
+time.sleep(0.5)
+
+
 def handle_data(data):
-  print(data)
-  datafname = "data_koule.csv"
+  print data,
+  datafname = "data_mrakomer.csv"
   with open(datafname, "a") as nbf:
     nbf.write(data)
   nbf.close()
 
-def read_from_port(ser):
-  connected = False
-  while not connected:
-    #serin = ser.read()
-    connected = True
-
-    while True:
-      reading = ser.readline().rstrip()
-      if (len(reading) > 0):
-        handle_data(str(time.time()) + ' ' + reading + '\n')
-
-thread = threading.Thread(target=read_from_port, args=(serial_port,))
-thread.start()
+while True:
+	reading = serial_port.readline().rstrip()
+	values = reading.split(' ')
+	if (len(values) == 18):
+		(t1, p1) = alt.get_tp()
+		#print values
+		handle_data(str(time.time()) + ' ' 
+		+ values[1] + ' ' 
+		+ str(round(t1,2)) + ' ' 
+		+ str(round(p1,1)) + ' ' 
+		+ str(round(float(values[2])/100,2)) + ' ' 
+		+ str(round(float(values[3])/100,2)) + ' ' 
+		+ str(round(float(values[4])/100,2)) + ' ' 
+		+ str(round(float(values[5])/100,2)) + ' ' 
+		+ values[6] + ' ' 
+		+ values[7] + ' ' 
+		+ values[8] + ' ' 
+		+ values[9] + ' ' 
+		+ values[10] + ' ' 
+		+ values[11] + ' ' 
+		+ values[12] + ' ' 
+		+ values[13] + ' ' 
+		+ values[14] + ' ' 
+		+ values[15] + ' ' 
+		+ values[16] + ' ' 
+		+ values[17] + '\n'
+		)
+		 
 
