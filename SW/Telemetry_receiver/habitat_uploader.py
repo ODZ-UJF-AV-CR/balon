18e09b2f54e35cc29c0e5a765657b1c243fbefd9
Jakub Kandera <jakubkandera@gmail.com>
2017-03-09 23:22:16 +0100

    Checksum function changed to crc16 and added function for creating sentences from NMEA format to format suitable for uploading to habitat database.

--- SW/Telemetry_receiver/habitat_uploader.py
+++ SW/Telemetry_receiver/habitat_uploader.py
@@ -2,37 +2,52 @@
 import sys
 import httplib
 import json
+import pynmea2
+import crc16
+import serial
 from base64 import b64encode
 from hashlib import sha256
 from datetime import datetime
+### testovaci knihovna
+import random
 
-def Fletcher16(string):
-    a = map(ord,string)
-    b = [sum(a[:i])%255 for i in range(len(a)+1)]
-    return (sum(b) << 8) | max(b)
-def Fletcher32(string):
-    a = map(ord,string)
-    b = [sum(a[:i])%65535 for i in range(len(a)+1)]
-    return (sum(b) << 16) | max(b)
-def Fletcher64(string):
-    a = map(ord,string)
-    b = [sum(a[:i])%4294967295 for i in range(len(a)+1)]
-    return (sum(b) << 32) | max(b)
+def make_sentence (sentence):
+    parsed = pynmea2.parse(sentence, False)
+    ### iterace vysky letu pro testovani (aby habitat databaze nehazela error kvuli identickym zpravam)
+    parsed.altitude = str(round(random.uniform(40000,1),1))
+    ###
+    if parsed.lat_dir == 'S':
+        parsed.lat = str(float(parsed.lat) * (-1))
+    if parsed.lon_dir == 'W':
+        parsed.lon = str(float(parsed.lon) * (-1))
+    new_sentence = "ODZUJF,%s,%s,%s,%s" % (parsed.timestamp, parsed.lat, parsed.lon, parsed.altitude)
+    new_sentence = "$$" + new_sentence + "*" + str(checksum(new_sentence)) + '\n'
+    return(new_sentence)
+
+def checksum(sentence):
+    crc = str(hex(crc16.crc16xmodem(sentence, 0xffff)))
+    crc_part = crc[2:6]
+    return(crc_part.upper())
 
 
-if len(sys.argv) < 2:
-    print "Usage: python %s <sentence> [recv callsign]" % sys.argv[0]
-    sys.exit()
+#if len(sys.argv) < 2:
+ #   print "Usage: python %s <sentence> [recv callsign]" % sys.argv[0]
+  #  sys.exit()
 
-#sentence = "$$pksbln,103,00:00:00,0.00000,0.00000,00000*D809"
-sentence = "$$BARC4,113,00:00:00,0.00000,0.00000,00000,0,0,0,20.6,4.1,175*D485"
+#pozdeji pro vstup ze seriove linky
+#ser = serial.Serial('/dev/ttyACM0', timeout=3)
+#data = ser.readline()
 
-if not sentence.endswith('\n'):
-    sentence += '\n'
+data = "$GPGGA,152225.00,5008.11103,S,01425.81157,W,2,07,1.25,317.1,M,44.3,M,,0000"
+
+sentence = make_sentence(data)
+
+print(sentence)
+
 
 sentence = b64encode(sentence)
 
-callsign = sys.argv[1] if len(sys.argv) > 2 else "HABTOOLS"
+callsign = "ODZUJF" #if len(sys.argv) > 2 else "HABTOOLS"
 
 date = datetime.utcnow().isoformat("T") + "Z"
 
