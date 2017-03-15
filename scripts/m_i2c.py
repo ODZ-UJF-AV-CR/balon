ec8f4e9df10283f70fb600275b09b02e3eedb34a
JSlegl <slegl.j@seznam.cz>
2017-03-15 13:41:04 +0100

    Adding sleep around altimet, port actualization

--- scripts/m_i2c.py
+++ scripts/m_i2c.py
@@ -19,18 +19,19 @@ import threading
 from pymlab import config
 
 import m_settings as g
+import m_nb
+import m_gps 
 
 # Mystic op
 NaN = float('nan')
 def isnan(x): return str(x) == 'nan'
 
 avg_cr = None      # Moving average climb rate
-data = {}
-data['Altimet_Climb'] = NaN
+g.data['Altimet_Climb'] = NaN
 
 #### Sensor Configuration ###########################################
 cfg_number = 0
-port = 4
+port = 1
 
 cfglist=[config.Config(i2c = {"port": port,},
 bus = [
@@ -40,8 +41,10 @@ bus = [
   
   "children": [
     {"name": "altimet", "type": "altimet01" , "channel": 0, },   
-    {"name": "sht25", "type": "sht25", "channel": 5, },
+    {"name": "sht25", "type": "sht25", "channel": 1, },
+    {"name": "sht25b", "type": "sht25", "channel": 7, },
     {"name": "gauge", "type": "lioncell", "channel": 3, },
+#    {"name": "lcd", "type": "i2clcd", "address": 0x27, "channel": 5, }
   ],
     },
 ],),]
@@ -54,8 +57,8 @@ except IndexError:
 
 # Get sensor value or -1 if not available
 def dv(sname):
-  if sname in data:
-    return(data[sname])
+  if sname in g.data:
+    return(g.data[sname])
   else:
     return(-1)
 
@@ -74,11 +77,60 @@ def press_to_height(pPa):
   else:
     return(NaN) 
 
+def lcdargs(lcdargs):
+  try:
+    cfg.initialize()
+    lcd = cfg.get_device("lcd")
+
+    lcd.route()
+    lcd.reset()
+    lcd.init()
+
+    row=1
+    logging.debug("LCD ARGS: " + ' '.join(lcdargs))
+    for lcdarg in lcdargs:
+      if row == 1:
+	time.sleep(0.1)
+	lcd.init()
+	time.sleep(0.1)
+	lcd.puts(lcdarg)
+	row = 2
+      else:
+	time.sleep(0.1)
+	lcd.set_row2()
+	time.sleep(0.1)
+	lcd.puts(lcdarg)
+	time.sleep(1)
+        row = 1
+  except IOError as e:
+    logging.error('LCD display not available: %s' % e)
+
+def lcd():
+  try:
+    cfg.initialize()
+    lcd = cfg.get_device("lcd")
+
+    lcd.route()
+    lcd.reset()
+    lcd.init()
+
+    time.sleep(0.1)
+    time.sleep(0.1)
+
+    lcd.puts('Bat %3.0f I %5.0f' % (dv('Bat_Charge'), -1*dv('Bat_AvgI')))
+    lcd.set_row2()
+    time.sleep(0.1)
+
+    lcd.puts('H1 %4.1f H2 %4.1f' % (dv('SHT_Hum'), dv('SHT_Hum2')))
+    #lcd.puts('H1 %4.1f H2 %4.1f' % (99.9, 99.9))
+    time.sleep(0.9)
+  except IOError as e:
+    logging.error('LCD display not available: %s' % e)
+
 #### i2c Data Logging ###################################################
 def get_i2c_data():
   global status
   global avg_cr
-  global data
   
   avg_over = 10.0    # Average over last ten measurements
   
@@ -89,16 +141,17 @@ def get_i2c_data():
   except IOError as e:
     logging.critical('Whole I2C bus unavailable: %s' % e)
 
-  if 'Epoch' in data and 'Altimet_Alt' in data:
+  if 'Epoch' in g.data and 'Altimet_Alt' in g.data:
     #logging.info('Setting olds')
-    data['Prev_Epoch'] = data['Epoch'] 
-    data['Prev_Alt'] = data['Altimet_Alt'] 
+    g.data['Prev_Epoch'] = g.data['Epoch'] 
+    g.data['Prev_Alt'] = g.data['Altimet_Alt'] 
 
-  data['Epoch'] = time.time()
+  g.data['Epoch'] = time.time()
 
   # Initialize 
   altimet = cfg.get_device("altimet")
   sht_sensor = cfg.get_device("sht25")
+  sht_sensor_2 = cfg.get_device("sht25b")
   gauge = cfg.get_device("gauge")
 
   time.sleep(0.5)
@@ -110,34 +163,32 @@ def get_i2c_data():
   logging.debug("Retrieving: Altimet temperature and pressure data")
   try:
     altimet.route()
+    time.sleep(0.1)
     (t1, p1) = altimet.get_tp()
-    data['Altimet_Temp'] = t1
-    data['Altimet_Press'] = p1
-    data['Altimet_Alt'] = press_to_height(p1) 
-    logging.info("AltiTemp: %.2f C  Press: %d Pa  Barometric height: %f m" % (t1, p1, data['Altimet_Alt']))
+    g.data['Altimet_Temp'] = t1
+    g.data['Altimet_Press'] = p1
+    g.data['Altimet_Alt'] = press_to_height(p1) 
+    logging.info("AltiTemp: %.2f C  Press: %d Pa  Barometric height: %f m" % (t1, p1, g.data['Altimet_Alt']))
 
     # Calculate moving average ascent rate
-    if (avg_cr == None) and not 'Prev_Alt' in data:
-      data['Prev_Alt'] = data['Altimet_Alt']
-    elif 'Prev_Alt' in data and 'Prev_Epoch' in data:
-      #logging.info('Alt {0} - Alt0 {1} / {2} {3}'.format(data['Altimet_Alt'], data['Prev_Alt'], data['Epoch'], data['Prev_Epoch']))
-      #logging.info('Delta t: {0}'.format(data['Epoch'] - data['Prev_Epoch']))
-      #logging.info('Delta h: {0}'.format(data['Altimet_Alt'] - data['Prev_Alt']))
-      climb = (data['Altimet_Alt'] - data['Prev_Alt'])/(data['Epoch'] - data['Prev_Epoch'])
+    if (avg_cr == None) and not 'Prev_Alt' in g.data:
+      g.data['Prev_Alt'] = g.data['Altimet_Alt']
+    elif 'Prev_Alt' in g.data and 'Prev_Epoch' in g.data:
+      climb = (g.data['Altimet_Alt'] - g.data['Prev_Alt'])/(g.data['Epoch'] - g.data['Prev_Epoch'])
       if (avg_cr == None):
         avg_cr = climb
       else:
-        #logging.info('avg_cr {0} avg_over {1} climb {2}'.format(avg_cr, avg_over, climb))
         avg_cr = (avg_cr*(avg_over - 1.0) + climb)/avg_over
-      data['Altimet_Climb'] = avg_cr
+      g.data['Altimet_Climb'] = avg_cr
     logging.info('Barometric climb rate: {0} m/s'.format(avg_cr))
     
   except IOError as e:
     logging.error('Altimet data unavailable %s' % e)
-    data.pop('Prev_Alt', None)
+    g.data.pop('Prev_Alt', None)
 
   csv_header = csv_header + 'T_Altimet\tPressure\tAlt_Alt\t'
   lr=lr+("%.3f\t%d\t%.1f\t" % (dv('Altimet_Temp'), dv('Altimet_Press'), dv('Altimet_Alt')))
+  time.sleep(0.1)
 
   # SHT sensor  
   logging.debug("Retrieving: SHT sensor data")
@@ -146,36 +197,52 @@ def get_i2c_data():
     temperature = sht_sensor.get_temp()
     humidity = sht_sensor.get_hum()
     logging.info("SHTTemp: %.2f C Humid: %.1f " % (temperature, humidity))
-    data['SHT_Temp'] = temperature
-    data['SHT_Hum'] = humidity
+    g.data['SHT_Temp'] = temperature
+    g.data['SHT_Hum'] = humidity
   except IOError as e:
     logging.error('SHT data unavailable as %s' % e)
 
   csv_header = csv_header + 'T_SHT\tHumidity\t'
   lr=lr+("%.2f\t%.1f\t" % (dv('SHT_Temp'), dv('SHT_Hum')))
+  
+  # SHT2 sensor  
+  logging.debug("Retrieving: SHT2 sensor data")
+  try:
+    sht_sensor_2.route()        
+    temperature = sht_sensor_2.get_temp()
+    humidity = sht_sensor_2.get_hum()
+    logging.info("SHTTemp2: %.2f C Humid2: %.1f " % (temperature, humidity))
+    g.data['SHT_Temp2'] = temperature
+    g.data['SHT_Hum2'] = humidity
+  except IOError as e:
+    logging.error('SHT 2 data unavailable as %s' % e)
+
+  csv_header = csv_header + 'T_SHT2\tHumidity2\t'
+  lr=lr+("%.2f\t%.1f\t" % (dv('SHT_Temp2'), dv('SHT_Hum2')))
 
   # Battery data
   logging.debug("Retrieving: Battery sensor data")
   try:
     gauge.route()
-    data['Bat_Temp'] = gauge.getTemp()
-    data['Bat_RemCap'] = gauge.getRemainingCapacity()
-    data['Bat_FullChargeCapacity'] = gauge.FullChargeCapacity()
-    data['Bat_V'] = gauge.Voltage()
-    data['Bat_AvgI'] = gauge.AverageCurrent()
-    data['Bat_Charge'] = gauge.StateOfCharge()
+    g.data['Bat_Temp'] = gauge.getTemp()
+    g.data['Bat_RemCap'] = gauge.getRemainingCapacity()
+    g.data['Bat_FullChargeCapacity'] = gauge.FullChargeCapacity()
+    g.data['Bat_V'] = gauge.Voltage()
+    g.data['Bat_AvgI'] = gauge.AverageCurrent()
+    g.data['Bat_Charge'] = gauge.StateOfCharge()
     logging.info("BatTemp: %.2f C RemCap: %d mAh FullCap: %d mAh U: %d mV I: %d mA Charge: %.2f %%" % 
-                    (data['Bat_Temp'], data['Bat_RemCap'], data['Bat_FullChargeCapacity'], data['Bat_V'], data['Bat_AvgI'], data['Bat_Charge']))
+                    (g.data['Bat_Temp'], g.data['Bat_RemCap'], g.data['Bat_FullChargeCapacity'], g.data['Bat_V'], g.data['Bat_AvgI'], g.data['Bat_Charge']))
   except IOError as e:
     logging.error('Battery data unavailable: %s' % e)
 
-  csv_header = csv_header + 'T_Bat\tRemCap_mAh\tCap_mAh\tU_mV\tI_mA\tCharge_pct'
-  lr=lr + ("%.2f\t%d\t%d\t%d\t%d\t%.2f" % (dv('Bat_Temp'), dv('Bat_RemCap'), dv('Bat_FullChargeCapacity'), dv('Bat_V'), dv('Bat_AvgI'), dv('Bat_Charge')))
+  csv_header = csv_header + 'T_Bat\tRemCap_mAh\tCap_mAh\tU_mV\tI_mA\tCharge_pct\t'
+  lr=lr + ("%.2f\t%d\t%d\t%d\t%d\t%.2f\t" % (dv('Bat_Temp'), dv('Bat_RemCap'), dv('Bat_FullChargeCapacity'), dv('Bat_V'), dv('Bat_AvgI'), dv('Bat_Charge')))
 
   status = {}
   status['header'] = csv_header
   status['record'] = lr
-  status['data'] = dict(data)
+  status['data'] = dict(g.data)
+
 
   return(status)
 
