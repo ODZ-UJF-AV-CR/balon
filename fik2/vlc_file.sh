eb5ce1c0c751df58b7c2cdb83f13f8c358f7ed64
Balon platform <vaclav.stepan@odz.ujf.cas.cz>
2017-03-16 18:17:24 +0000

    add fik2/vlc_file.sh

--- fik2/vlc_file.sh
+++ fik2/vlc_file.sh
@@ -15,6 +15,6 @@ else
   v4l2-ctl -d ${DEVICE} --set-fmt-video=width=1920,height=1080,pixelformat=1
   v4l2-ctl -d ${DEVICE} --set-ctrl=focus_auto=0
   v4l2-ctl -d ${DEVICE} --set-ctrl=focus_absolute=0
-  cvlc v4l2:///dev/video${DEVICE}:chroma=h264:width=1920:height=1080:fps=30 --sout $OUTF
+  time cvlc v4l2:///dev/video${DEVICE}:chroma=h264:width=1920:height=1080:fps=30 --sout $OUTF
 fi
 
