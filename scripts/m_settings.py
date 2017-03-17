946e7740d88e4de142564379a5e03a1644310f73
Balon platform <vaclav.stepan@odz.ujf.cas.cz>
2017-03-17 17:56:03 +0000

    set conf for flight

--- scripts/m_settings.py
+++ scripts/m_settings.py
@@ -6,13 +6,13 @@ image_dir = data_dir + 'img/'
 
 round_beat = 5
 
-alt_radio_off = 300
-alt_radio_on = 280
+alt_radio_off = 8000
+alt_radio_on = 7000
 
 sms_delay_min = 15
-sms_delay_max = 300
+sms_delay_max = 3*60
 
-alt_diff_sms_trigger = 20
+alt_diff_sms_trigger = 500
 
 sms_phone_number = "00420775268014"
 
