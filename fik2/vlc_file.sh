4b30bbde5acc4b0fad947086f7c9e0c0ee028fc0
Balon platform <vaclav.stepan@odz.ujf.cas.cz>
2017-03-17 14:46:09 +0000

    make sure video folder exists

--- fik2/vlc_file.sh
+++ fik2/vlc_file.sh
@@ -15,6 +15,7 @@ else
   v4l2-ctl -d ${DEVICE} --set-fmt-video=width=1920,height=1080,pixelformat=1
   v4l2-ctl -d ${DEVICE} --set-ctrl=focus_auto=0
   v4l2-ctl -d ${DEVICE} --set-ctrl=focus_absolute=0
+  mkdir -p /data/balon/video
   time cvlc v4l2:///dev/video${DEVICE}:chroma=h264:width=1920:height=1080:fps=30 --sout $OUTF
 fi
 
