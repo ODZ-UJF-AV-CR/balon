2f1b11ad36ab42c60c7158f499c1c869b1860571
Balon platform <vaclav.stepan@odz.ujf.cas.cz>
2017-03-17 18:18:52 +0000

    fix scripts/clean_data.sh

--- scripts/clean_data.sh
+++ scripts/clean_data.sh
@@ -4,8 +4,10 @@ echo -n Remove all logs and img dir in: ${data_dir} [y/n]?\
 read r
 if [ x$r == "xy" ]; then
   cd $data_dir || exit 1
-  rm -v data_*.csv monitor-*.log 
-  rm -vrf img videos
+  rm -v data_*.csv
+  rm -v monitor-*.log 
+  rm -vrf img
+  rm -vrf videos
 else
   echo Ok, not removing anything.
 fi
