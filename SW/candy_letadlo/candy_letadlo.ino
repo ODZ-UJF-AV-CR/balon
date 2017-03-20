684f2e7469a621e129024bf093df7232aaf44b80
kakl <martin.kakona@gmail.com>
2017-03-20 20:35:33 +0100

    CANDY with GPS with power sawing

--- SW/candy_letadlo/candy_letadlo.ino
+++ SW/candy_letadlo/candy_letadlo.ino
@@ -8,6 +8,11 @@ CMD MOSI 5 B5
 DAT0 MISO 6 B6
 CLK SCK 7 B7
 
+SERIAL 2
+--------
+RX 18 PC2
+TX 19 PC3
+
 The following needs to be added to the line mentioning the atmega644 in
 /usr/share/arduino/libraries/SD/utility/Sd2PinMap.h:
 
@@ -15,16 +20,19 @@ The following needs to be added to the line mentioning the atmega644 in
 
 so that it reads:
 
-    #elif defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644__)|| defined(__AVR_ATmega1284P__)
-    
- 
+    #elif defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644__)|| defined(__AVR_ATmega1284P__) 
 */
 
 #include <SD.h>
 #include "wiring_private.h"
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(18, 19); // RX, TX
+#define MSG_NO 20    // number of logged NMEA messages
+
+#define RELE_ON   11    // PD3
+#define RELE_OFF  12    // PD4
+
+SoftwareSerial swSerial(18, 19); // RX, TX
 
 
 const int chipSelect = 4;
@@ -41,7 +49,7 @@ void setup()
     ; // wait for serial port to connect. Needed for Leonardo only
    }
 
-  mySerial.begin(9600);
+  swSerial.begin(9600);
   //mySerial.println("#Cvak...");
 
   Serial.print("#Initializing SD card...");
@@ -83,119 +91,183 @@ void setup()
   ADMUX = (analog_reference << 6) | ((pin | 0x10) & 0x1F);
 
   pinMode(RESET, OUTPUT);     
+  pinMode(RELE_ON, OUTPUT);
+  pinMode(RELE_OFF, OUTPUT);
+  
+  digitalWrite(RELE_ON, LOW);  // Rele switch OFF
+  digitalWrite(RELE_OFF, HIGH);  
+  delay(500);
+  digitalWrite(RELE_OFF, LOW);
 }
 
 int oldValue = 0;  // Variable for filtering death time double detection
 
 void loop()
 {
-  uint8_t low, high;
-  int sensor;
-  uint16_t buffer[1024];
-  
-  for(int n=0; n<1024; n++)
+  for(int x=0; x<((5*60)/11); x++)  // 5 minutes of radiation measurement
   {
-    buffer[n]=0;
-  }
-  
-  digitalWrite(RESET, HIGH);   // Reset peak detector
-  digitalWrite(RESET, HIGH);   // Reset peak detector
-  digitalWrite(RESET, HIGH);   // Reset peak detector
-  digitalWrite(RESET, HIGH);   // Reset peak detector
-  digitalWrite(RESET, HIGH);   // Reset peak detector
-  digitalWrite(RESET, LOW); 
-  for(int n=0; n<14000; n++) // cca 10 s
-  {
-    // start the conversion
-    sbi(ADCSRA, ADSC);
-    // ADSC is cleared when the conversion finishes
-    while (bit_is_set(ADCSRA, ADSC));  // conversion cca 100 us
+    uint8_t lo, hi;
+    int sensor;
+    uint16_t buffer[1024];
+    
+    for(int n=0; n<1024; n++)
+    {
+      buffer[n]=0;
+    }
+    
+    digitalWrite(RESET, HIGH);   // Reset peak detector
+    digitalWrite(RESET, HIGH);   // Reset peak detector
+    digitalWrite(RESET, HIGH);   // Reset peak detector
+    digitalWrite(RESET, HIGH);   // Reset peak detector
     digitalWrite(RESET, HIGH);   // Reset peak detector
     digitalWrite(RESET, LOW); 
-   
-    
-    // we have to read ADCL first; doing so locks both ADCL
-    // and ADCH until ADCH is read.  reading ADCL second would
-    // cause the results of each conversion to be discarded,
-    // as ADCL and ADCH would be locked when it completed.
-    low  = ADCL;
-    high = ADCH;
-    
-    // combine the two bytes
-    sensor = (high << 8) | low;
-  
-    // arrange integer value (read ATMega 2560 Datashet p.288) figure 26-15
-    if (sensor > 511 )
+    for(int n=0; n<14000; n++) // cca 10 s
     {
-      sensor -= 1023 ;
+      // start the conversion
+      sbi(ADCSRA, ADSC);
+      // ADSC is cleared when the conversion finishes
+      while (bit_is_set(ADCSRA, ADSC));  // conversion cca 100 us
+      digitalWrite(RESET, HIGH);   // Reset peak detector
+      digitalWrite(RESET, LOW); 
+     
+      
+      // we have to read ADCL first; doing so locks both ADCL
+      // and ADCH until ADCH is read.  reading ADCL second would
+      // cause the results of each conversion to be discarded,
+      // as ADCL and ADCH would be locked when it completed.
+      lo  = ADCL;
+      hi = ADCH;
+      
+      // combine the two bytes
+      sensor = (hi << 8) | lo;
+    
+      // arrange integer value (read ATMega 2560 Datashet p.288) figure 26-15
+      if (sensor > 511 )
+      {
+        sensor -= 1023 ;
+      }
+      //sensor +=30; // Add offset to ground
+      if ((sensor>=0)&&(buffer[sensor]<65535)&&(oldValue<sensor)) buffer[sensor]++;
+      oldValue = sensor;
+      for (int i=0; i<100; i++) {digitalWrite(RESET, LOW);} // integration cca 500 us
     }
-    //sensor +=30; // Add offset to ground
-    if ((sensor>=0)&&(buffer[sensor]<65535)&&(oldValue<sensor)) buffer[sensor]++;
-    oldValue = sensor;
-    for (int i=0; i<100; i++) {digitalWrite(RESET, LOW);} // integration cca 500 us
-  }
+    
   
-  // make a string for assembling the data to log:
-  String dataString = "";
-  String toModem = "";
-
-  dataString += String(count); 
-  dataString += ",";
-
-  for(int n=0; n<(511+31); n++)
-  {
-    dataString += String(buffer[n]); 
-    dataString += ",";
-  }
-
-  int noise = 0;
-  for(int n=0; n<(511+31); n++)
-  {
-    if(buffer[n]==0)
     {
-      noise = n;
-      break;
+      // make a string for assembling the data to log:
+      String dataString = "$CANDY,";
+      //String toModem = "";
+  
+      dataString += String(count); 
+      dataString += ",";
+    
+      for(int n=0; n<(511+31); n++)
+      {
+        dataString += String(buffer[n]); 
+        dataString += ",";
+      }
+    
+      int noise = 0;
+      for(int n=0; n<(511+31); n++)
+      {
+        if(buffer[n]==0)
+        {
+          noise = n;
+          break;
+        }
+      }
+      
+      int flux=0;
+      for(int n=noise; n<(511+31); n++)
+      {
+        flux += buffer[n];
+      }
+    
+      int loDose = 0;
+      if (noise > 0) loDose=buffer[noise-1];
+    
+      int hiDose=0;
+      for(int n=noise+20; n<(511+31); n++)
+      {
+        hiDose += buffer[n];
+      }
+      
+      //toModem = "$"+ String(count++) + "," + String(noise) + "," + String(flux) + "," + String(loDose) + "," + String(hiDose) + "*";
+      
+      //mySerial.println(toModem);
+      dataString += String(noise) + "," + String(flux) + "," + String(loDose) + "," + String(hiDose);
+    
+      // open the file. note that only one file can be open at a time,
+      // so you have to close this one before opening another.
+      File dataFile = SD.open("datalog.txt", FILE_WRITE);
+    
+      // if the file is available, write to it:
+      if (dataFile) 
+      {
+        dataFile.println(dataString);  // write to SDcard
+        Serial.println(dataString);    // print to terminal
+        dataFile.close();
+      }  
+      // if the file isn't open, pop up an error:
+      else 
+      {
+        Serial.println("#error opening datalog.txt");
+      }
+    }  
+  }
+  
+  {
+    // make a string for assembling the NMEA to log:
+    String dataString = "";
+    char incomingByte;
+  
+    digitalWrite(RELE_OFF, LOW);  // Rele switch ON
+    digitalWrite(RELE_ON, HIGH);  
+    delay(500);
+    digitalWrite(RELE_ON, LOW);
+      
+    delay(26000); // Delay to fix  
+    
+    boolean flag = false;
+    int messages = 0;
+    for(int n=0; n<30000; n++)
+    {
+      if (Serial.available() > 0) 
+      {
+        // read the incoming byte:
+        incomingByte = Serial.read();
+        
+        if (incomingByte == '$') {flag = true; messages++;};
+        
+        // say what you got:
+        if (flag && (messages<=MSG_NO)) dataString+=incomingByte;
+      }
+      for (int i=0; i<50; i++) {digitalWrite(RESET, LOW);} // delay cca 250 us
+      if (messages > MSG_NO) break;
     }
-  }
-  
-  int flux=0;
-  for(int n=noise; n<(511+31); n++)
-  {
-    flux += buffer[n];
-  }
 
-  int loDose = 0;
-  if (noise > 0) loDose=buffer[noise-1];
-
-  int hiDose=0;
-  for(int n=noise+20; n<(511+31); n++)
-  {
-    hiDose += buffer[n];
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  
+    // if the file is available, write to it:
+    if (dataFile) 
+    {
+      dataFile.print(dataString);  // write to SDcard
+      Serial.print(dataString);    // print to terminal
+      dataFile.close();
+    }  
+    // if the file isn't open, pop up an error:
+    else 
+    {
+      Serial.println("#error opening datalog.txt");
+    }
+    
+    digitalWrite(RELE_ON, LOW);  // Rele switch OFF
+    digitalWrite(RELE_OFF, HIGH);  
+    delay(500);
+    digitalWrite(RELE_OFF, LOW);
   }
-  
-  toModem = "$"+ String(count++) + "," + String(noise) + "," + String(flux) + "," + String(loDose) + "," + String(hiDose) + "*";
-  
-  mySerial.println(toModem);
-  dataString += String(noise) + "," + String(flux) + "," + String(loDose) + "," + String(hiDose);
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
-
-  // if the file is available, write to it:
-  if (dataFile) 
-  {
-    dataFile.println(dataString);  // write to SDcard
-    Serial.println(dataString);    // print to terminal
-    dataFile.close();
-  }  
-  // if the file isn't open, pop up an error:
-  else 
-  {
-    Serial.println("#error opening datalog.txt");
-  }
-  
-  delay(1000);
 }
 
 
