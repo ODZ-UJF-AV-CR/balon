8d3b19aee71b14190d0f928b9f947ff05e3e8c33
Martin Povišer <povik@protonmail.com>
2019-11-03 21:40:01 +0100

    Add script to read telemetry test logs
    In the test, a beacon was transmitting timestamp messages while
    switching modulation settings in an absolute-time-dependent manner.
    A receiver followed the modulation changes and saved the received
    messages.
    
    These scripts convert timestamps to the modulation settings applied at
    the time.

---
+++ data/191103_telemetry_test/read_rec2.py
@@ -0,0 +1,28 @@
+import math
+import sys
+
+def fmt_settings(vals):
+	return " ".join(["%s=%s" % m for m in vals])
+
+AIRRATES = [2, 4, 8, 16] #, 19, 24, 32, 64, 96, 128, 192, 250]
+schedule = [
+	[("AIR_SPEED", air_speed), ("ECC", ecc)]
+		for air_speed in AIRRATES
+		for ecc in [0, 1]
+]
+
+with open(sys.argv[1], "rb") as f:
+	for l in f:
+		try:
+			l = str(l, "ascii")
+		except:
+			continue
+		if l[:2] != "t=":
+			continue
+		t = int(l[2:])
+		if t < 1500000000 or t > 1600000000:
+			continue
+		print(
+			t, fmt_settings(
+				schedule[(t//10)%len(schedule)]
+		))
