59a2cbedd65c513099bed2bd1eeaccff5d3a867f
Roman Dvořák <romandvorak@mlab.cz>
2019-11-28 23:23:51 +0100

    nove prvky

--- SW/Telemetry_receiver/habitat_uploader.py
+++ SW/Telemetry_receiver/habitat_uploader.py
@@ -17,23 +17,22 @@ from datetime import datetime
 import Queue
 from threading import Thread
 
-callsign = "LetFik5"
-
-if len(sys.argv) < 3: # Terminate program, if run without defining port as an argument
-    print "Usage: python %s [recv port] [receiver callsign]" % sys.argv[0]
-    sys.exit()
 
 ###################################################################
+
+callsign = "LetFik5"
+virtual = True;
+
+###################################################################
+
 
 
 def make_sentence(sentence, checksum_bool): # Function which takes NMEA sentence as an argument
                                             # and returns sentence suitable for uploading to DB
 
     # Creates new sentence in format configured on Habitat's webpage
-    new_sentence = callsign + ",{},{},{},{:0.0f},{}".format(sentence['timestamp'], sentence['lat'], sentence['lon'], sentence['altitude'], sentence['num_sats'])
+    new_sentence = callsign + ",{},{},{},{},{},{},{}".format(sentence['timestamp'], sentence['lat'], sentence['lon'], sentence['altitude'], sentence['num_sats'], sentence['fix'], sentence['temperature'])
     new_sentence = "$$" + new_sentence + "*" + str(checksum(new_sentence)) + '\n'
-    #new_sentence = '$$LetFik5,123,13:16:24,51.123,0.123,11000*ABCD'
-    print(new_sentence)
     return(new_sentence)
 
 
@@ -49,8 +48,6 @@ def make_data(sentence, callsign): # Creates data in format suitable for upload 
             sys.argv[2]: {
                 "time_created": get_date(True),
                 "time_uploaded": get_date(True),
-                # "time_created": date,
-                # "time_uploaded": date
                 },
             },
     }
@@ -68,20 +65,16 @@ def get_date(format_bool):
     else:
         return datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
+if len(sys.argv) < 3: # Terminate program, if run without defining port as an argument
+    print "Usage: python %s [recv port] [receiver callsign]" % sys.argv[0]
+    sys.exit()
 
-timestamp = '00:01:01'
-latitude = 0.00
-longtitude = 0.00
-altitude = -1
-timestamp = datetime.utcnow().strftime("%H:%M:%S")
-num_sats = -1
+
 date = get_date(False)
 
 q = Queue.Queue(10)
-#mav = mavutil.mavlink_connection(sys.argv[1], baud=9600, source_system=255)
-mav = None
 
-def get_mavlink(q, mav):
+def get_mavlink(q):
     mav = mavutil.mavlink_connection(sys.argv[1], baud=57600, source_system=255)
 
     while True:
@@ -90,7 +83,7 @@ def get_mavlink(q, mav):
             #print(data)
             data = data.to_dict()
             if data['mavpackettype'] == "GPS_RAW_INT":
-                print(data)
+                #print(data)
                 q.put({
                     'lat': data['lat']/10e6,
                     'lon': data['lon']/10e6,
@@ -103,11 +96,10 @@ def get_mavlink(q, mav):
         except Exception as e:
             print("task error:", e)
 
-worker = Thread(target=get_mavlink, args=(q, mav))
+worker = Thread(target=get_mavlink, args=(q))
 worker.setDaemon(True)
 worker.start()
 
-virtual = True;
 
 try:
     while True: # Infinite loop waiting for data from configured serial port
@@ -122,6 +114,8 @@ try:
                     'lon': data['lon'],
                     'altitude': data['alt'],
                     'num_sats': data.get('num_sats', -1),
+                    'fix': -1,
+                    'temperature': 10,
                 }
 
             else:
@@ -130,13 +124,13 @@ try:
                     'timestamp': datetime.utcnow().strftime("%H:%M:%S"),
                     'lat': 50.12,
                     'lon': 14.5,
-                    'altitude': 200,
+                    'altitude': 200.34,
                     'num_sats': 0,
+                    'fix': -1,
+                    'temperature': 10,
                 }
 
-
             sentence = make_sentence(param, True)
-
             print("Sending: " + sentence)    # Prints sentence uploading to DB to the terminal              
             
             for x in range(0,4):
@@ -157,8 +151,6 @@ try:
                     )
 
                     response = c.getresponse() # Prints response from DB
-                    #print(response.read())
-
 
                 except Exception as e:
                     if x < 3:
@@ -171,11 +163,7 @@ try:
                 
         except Exception as e: # Close safely in case of port disconnection
             print("Error:", e)
-        #time.sleep(0.5)
+
 
 except KeyboardInterrupt: # Close safely with "CTRL + C"
     print("_Program closed\n")
-
-#printer.close()
-#printer_raw.close()
-#printer_candy.close()
